4b9766ddbef9aadaf9b8add71c92420aade359ca
n mnn <153140996+pretzer25@users.noreply.github.com>
2024-03-15 22:26:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ function myFunction_reload() {
 }
 
 function successall() {
-  localStorage.setItem('islogeddd', 'yes');
+  localStorage.setItem('islogeddd', user_email);
   window.location.href = "/./welcome.html";
 }
 
